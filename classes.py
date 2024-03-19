b3c0aa8491cbcfb89ef89e974b85b57e4938edd8
L2530L <elagrosa23A-0057@olopsc.edu.ph>
2024-03-19 16:01:56 +0800

    trying

--- classes.py
+++ classes.py
@@ -1,5 +1,5 @@
 class User:
-    def __init__(self, first_name, middle_name, last_name, password, user_money):
+    def __init__(self, first_name, middle_name, last_name, password, user_money:int):
         self.__first_name = first_name
         self.__middle_name = middle_name
         self.__last_name = last_name
@@ -21,20 +21,20 @@ class Bank:
         self.money_balance = money_balance
 
     #withraw function change name ng attributes kapag mali
+    """
     def withdraw(self, user, amount):
-        user.user_money += amount
+        user.get_user_money() += amount
         self.money_balance -= amount
 
     #deposit to
     def deposite(self, user, amount):
-        user.user_money -= amount
-        self.money_balance += amount
-
-    def __init__(self, money_balance):
-        self.money_balance = money_balance
+        user.get_user_money() -= amount
+        self.money_balance += amount"""
 
 b1 = Bank(money_balance=100)
-u1 = User(first_name='Juan', middle_name= 'J.', last_name='Tamad', password= 123, user_money= 50)
+u1 = User(first_name='Juan', middle_name= 'J.', last_name='Tamad', password= 123, 50)
+
+print(u1.get_user_money())
 
 #withdraw kuha or plus
 #deposit minus or bawas
